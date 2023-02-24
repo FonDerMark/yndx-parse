cbc9cb7be85771b4ac0070666dab0cb49874cc10
xm0rph <publicnox@gmail.com>
2023-02-24 11:07:44 +0500

    v.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='yndx_parse',
-    version='0.6',
+    version='0.10',
     packages=['yndx_parse'],
     url='https://github.com/FonDerMark/yndx-parse',
     license='MIT',
